3ad47a3e37dd17b9c0d61c6e34fdbbf0bc541601
nhkmi1001 <nhkmi1001@gmail.com>
2022-11-15 09:28:18 +0900

    submit

--- users/models.py
+++ users/models.py
@@ -9,7 +9,6 @@ class UserManager(BaseUserManager):
         user = self.model(
             username=username,
         )
-        print("가입")
         user.set_password(password)
         user.save(using=self._db)
         return user
